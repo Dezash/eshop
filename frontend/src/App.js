415548a8438f102d136e768a21e31a0a4a122ffa
Gabrielius Laurinavičius <drakath13@gmail.com>
2020-11-09 21:03:57 +0200

    Add password recovery page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import SignUp from './components/Auth/SignUp';
+import Recovery from './components/Auth/Recovery';
 
 function App() {
   return (
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/forgot_password" component={Recovery} />
       <Route exact path="/" component={Login} />
     </Switch>
   );
--- frontend/src/components/Auth/Recovery.jsx
+++ frontend/src/components/Auth/Recovery.jsx
@@ -46,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Login() {
+export default function Recovery() {
   const classes = useStyles();
 
   return (
@@ -57,7 +57,7 @@ export default function Login() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Recover account
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
@@ -71,21 +71,6 @@ export default function Login() {
             autoComplete="email"
             autoFocus
           />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
@@ -93,20 +78,8 @@ export default function Login() {
             color="primary"
             className={classes.submit}
           >
-            Sign In
+            Recover
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="/forgot_password" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
         </form>
       </div>
       <Box mt={8}>
