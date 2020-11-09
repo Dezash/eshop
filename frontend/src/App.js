ddc7829342de660428c61a3fbcaf98b0575b1bcc
Gabrielius Laurinavičius <drakath13@gmail.com>
2020-11-09 22:13:37 +0200

    Add reset password page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import SignUp from './components/Auth/SignUp';
 import Recovery from './components/Auth/Recovery';
+import Reset from './components/Auth/Reset';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
       <Route path="/forgot_password" component={Recovery} />
+      <Route path="/reset_password" component={Reset} />
       <Route exact path="/" component={Login} />
     </Switch>
   );
