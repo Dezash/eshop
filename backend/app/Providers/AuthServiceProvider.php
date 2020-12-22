be56fd84ec9204d38ebc793b73a9723f9e154e9a
Tomas Butkus <tomasbutkus@outlook.com>
2020-12-22 04:01:34 +0200

    fix

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Team;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Gate::define('seller', function($user){
-            return $user->currentTeam == 2;
+            return $user->currentTeam->id == 2;
         });
         //
     }
