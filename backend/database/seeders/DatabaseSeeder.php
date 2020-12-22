3ea36440138d9a64074977671266c34f594b8105
Gabrielius Laurinavičius <drakath13@gmail.com>
2020-12-22 03:38:48 +0200

    Seed teams

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(ImageSeeder::class);
+        $this->call(TeamSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
